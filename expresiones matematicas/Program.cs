925d1d9147b74c292633c932dce4f64eb1cf20a7
Enzo B <enzobustillos@hotmail.com>
2023-11-13 12:33:46 -0400

    Agregar archivos de proyecto.

---
+++ expresiones matematicas/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+namespace expresiones_matematicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            validacion();
+
+
+
+        }
+
+
+         static void validacion()
+        { 
+            string expresion;
+            int parentesis = 0,operando=0,operador=0;
+            string variable="";
+            List<string> operadores = new List<string>() { "+","-","*","^","/" };
+           
+
+
+            Console.WriteLine("Expresion que desea transformar a postfija");
+            expresion = Console.ReadLine();
+
+
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                if (char.IsNumber(expresion[i]))
+                {
+                    Console.WriteLine("Error la expesion contiene numeros no se puede procesar");
+                    Console.ReadKey();
+                    
+
+                }
+
+
+                variable = Convert.ToString(expresion[i]);
+
+                if (variable == "(") { parentesis++;continue; }
+
+                if (variable == ")") { parentesis--;continue; }
+
+                if (char.IsLetter(expresion[i])) { operando++;continue; }
+
+                if (operadores.IndexOf(variable) >= 0) { operador++;continue; }
+
+            }
+
+            if (parentesis==0 && operador == operando-1)
+            {
+
+                Console.WriteLine("La expresion esta bien escrita");
+                Console.ReadKey();
+                postfija(expresion);
+            }
+            else
+            {
+                Console.WriteLine("La expresion no es mal escrita");
+                Console.ReadKey();
+            }
+
+          }
+
+        static void postfija(string expresion)
+        {
+            string exprepost = "";
+            string operando1 = "";
+            string operando2 = "";
+            string operador1 = "";
+            string operador2 = "";
+            string salida = "";
+            string expreint = "";
+            List<string> operadores = new List<string>() { "+", "-", "*", "^", "/" };
+
+            pila expresion_pila = new pila();
+
+            for (int i = expresion.Length-1; i >=0; i--)
+            {
+
+                expresion_pila.push(expresion[i]);
+
+
+            }
+
+            while (true)
+            {
+                salida =Convert.ToString( expresion_pila.pop());
+                
+                if (char.IsLetter(char.Parse(salida)))
+                {
+                    operando1 = salida;     
+                    operador1 = Convert.ToString(expresion_pila.pop());
+                    operando2 = Convert.ToString(expresion_pila.pop());
+
+                    if (operadores.IndexOf(operador1) == -1)
+                    {
+                        Console.WriteLine("La expresion esta mal escrita");
+                        Console.ReadKey();
+                    }
+
+                    expreint =expreint+ operando1 + operando2 + operador1 + operador2;
+
+                    operador2 = "";
+
+                }
+
+                if (operadores.IndexOf(salida)>=0)
+                {
+                    operador2 = salida;
+                
+                }
+
+
+                if (Convert.ToInt64( expresion_pila.poptope())==-1)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine(expreint);
+        }
+
+     }
+}
+
+
+
---
+++ expresiones matematicas/pila.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expresiones_matematicas
+{
+    internal class pila
+    {
+        #region atributos
+        protected const int max = 50;
+        object[] elementos;
+        int tope;
+        #endregion
+
+        public pila()
+        {
+            tope = -1;
+            elementos = new object[max];
+        }
+
+
+        private bool vacio()
+        {
+            return (tope == -1);
+        }
+
+        private bool lleno()
+        {
+            return (tope == max - 1);
+        }
+
+        public void push(object dato)
+        {
+            if (lleno())
+            {
+                Console.WriteLine("Error. Pila llena");
+                Console.ReadKey();
+            }
+            else
+            {
+                tope++;
+                elementos[tope] = dato;
+
+            }
+        }
+
+        public object pop()
+        {
+            if (vacio())
+            {
+                Console.WriteLine("Error. Pila vacia.");
+                Console.ReadKey();
+                return (null);
+            }
+            else
+            {
+                object D;
+                D = elementos[tope];
+                tope--;
+                return (D);
+            }
+        }
+
+        public void imprimir()
+        {
+            if (vacio())
+            {
+                Console.WriteLine("\nError. pila vacia.");
+                Console.ReadKey();
+            }
+            else
+            {
+                object d;
+                pila pa = new pila();
+                while (!vacio())
+                {
+                    d = pop();
+                    Console.WriteLine(d);
+                    pa.push(d);
+                }
+
+                while (!pa.vacio())
+                {
+                    push(pa.pop());
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        public object poptope()
+        {
+            if (vacio())
+            {
+                //Console.WriteLine("Error. Pila vacia.");
+                //Console.ReadKey();
+                return (-1);
+            }
+            else
+            {
+                object D;
+                D = elementos[tope];
+                return (D);
+            }
+        }
+
+        public int count()
+        {
+            return tope + 1;
+        }
+
+        public void clear()
+        {
+            tope = -1;
+        }
+    }
+}
